702d90c2459936e4f210e5c8a9523bd27ea17fc2
Pol frx <pol.2frx@hotmail.fr>
2020-01-07 16:24:18 +0100

    sample for db 2

--- api/eHealthLib/main_bdd.cpp
+++ api/eHealthLib/main_bdd.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
 
-#include <CkHttpRequest.h>
-#include <CkJsonObject.h>
-#include <CkJsonArray.h>
+#include <stdio.h>
+#include <curl/curl.h>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
     for (i=0; i<=10; i=i+1) {
-        CkHttpRequest req;
+        CURL *curl;
+        CURLcode res;
 
-        req.put_HttpVerb("POST");
-        req.put_Path("localhost/api/post/");
-        req.put_ContentType("application/json");
+        curl_global_init(CURL_GLOBAL_ALL);
+        curl = curl_easy_init();
+        if(curl)
+        {
+            int res = 0;
+            snprintf(curl_url, sizeof(curl_url), "http://localhost/api/post", results);
+            snprintf(curl_fields, sizeof(curl_fields),"{\"sensor_id\":\"%d\", \"value\":\"%d\"}", 1, i);
 
-        req.AddHeader("Accept","application/json");
 
-        CkJsonObject json;
-        json.AppendString("sensor_id", 1);
-        json.AppendString("value", i);
-        req.LoadBodyFromString(json.emit(),"utf-8");
+            struct curl_slist *headers = NULL;
+            headers = curl_slist_append(headers, "Accept: application/json");
+            headers = curl_slist_append(headers, "Content-Type: application/json");
+            headers = curl_slist_append(headers, "charsets: utf-8");
+
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(curl, CURLOPT_URL, curl_url);
+            curl_easy_setopt(curl, CURLOPT_POST, 1);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, curl_fields);
+
+            res = curl_easy_perform(curl);
+
+            curl_easy_cleanup(curl);
+            curl_global_cleanup();
+        }
     }
 }
