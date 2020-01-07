51eed29bb519b1b98dd194a0b22eb6729d49c060
Pol frx <pol.2frx@hotmail.fr>
2020-01-07 14:10:04 +0100

    sample to test pulsio

--- api/eHealthLib/main.cpp
+++ api/eHealthLib/main.cpp
@@ -6,9 +6,20 @@
 using namespace std;
 using namespace pqxx;
 
-
-
 int main(int argc, char* argv[]) {
+    eHealthClass eHealth();
+    eHealth->initPulsioximeter();
+
+    while(true) {
+        int oxy = eHealth->getOxygenSaturation();
+        int bpm = eHealth->getBPM();
+        cout << "Oxy: " << oxy << endl;
+        cout << "BPM: " << bpm << endl;
+        sleep(2000);
+    }
+}
+
+int utils() {
     eHealthClass eHealth();
 
     eHealth->initBloodPressureSensor();
@@ -17,15 +28,16 @@ int main(int argc, char* argv[]) {
     while(true) {
        int airFlow = eHealth->getAirFlow();
        int electroCardio = eHealth->getECG();
-       float pulsio = eHealth->getECG();
+       int oxy = eHealth->getOxygenSaturation();
+       int bpm = eHealth->getBPM();
 
-       insertIntoDb(airFlow, electroCardio, pulsio);
+       insertIntoDb(airFlow, electroCardio, oxy, bpm);
 
        sleep(10000);
     }
 }
 
-void insertIntoDb(int airFlow, int electroCardio, float pulsio) {
+void insertIntoDb(int airFlow, int electroCardio, int oxy, int bpm) {
     std::string sql;
 
    try {
@@ -38,16 +50,32 @@ void insertIntoDb(int airFlow, int electroCardio, float pulsio) {
       }
 
       /* Create SQL statement */
-       sql = "INSERT INTO api_point (sensor_id, value) VALUES (1," + airFlow + "); ";
-       sql = "INSERT INTO api_point (sensor_id, value) VALUES (2," + electroCardio + "); ";
-       sql = "INSERT INTO api_point (sensor_id, value) VALUES (3," + pulsio + "); ";
+       sql1 = "INSERT INTO api_point (sensor_id, value) VALUES (1," + airFlow + "); ";
+       sql2 = "INSERT INTO api_point (sensor_id, value) VALUES (2," + electroCardio + "); ";
+       sql3 = "INSERT INTO api_point (sensor_id, value) VALUES (3," + oxy + "); ";
+       sql4 = "INSERT INTO api_point (sensor_id, value) VALUES (4," + bpm + "); ";
 
       /* Create a transactional object. */
       work W(C);
-
       /* Execute SQL query */
-      W.exec( sql );
+      W.exec( sql1 );
       W.commit();
+
+      work W(C);
+      /* Execute SQL query */
+      W.exec( sql2 );
+      W.commit();
+
+      work W(C);
+      /* Execute SQL query */
+      W.exec( sql3 );
+      W.commit();
+
+      work W(C);
+      /* Execute SQL query */
+      W.exec( sql4 );
+      W.commit();
+
       cout << "Records created successfully" << endl;
       C.disconnect ();
    } catch (const std::exception &e) {
