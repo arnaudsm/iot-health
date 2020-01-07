569552af31c32ef072c9a3760f15b460ddfbd606
Pol frx <pol.2frx@hotmail.fr>
2020-01-07 16:51:35 +0100

    sample for db 2

--- api/eHealthLib/main_bdd.cpp
+++ api/eHealthLib/main_bdd.cpp
@@ -8,36 +8,29 @@ using namespace std;
 int main(int argc, char* argv[]) {
     int i;
     for (i=0; i<=10; i=i+1) {
-        CURL *curl;
-        CURLcode res;
+          CURL *curl;
+          CURLcode res;
 
-        curl_global_init(CURL_GLOBAL_ALL);
-        curl = curl_easy_init();
-        if(curl)
-        {
-            int res = 0;
+          /* In windows, this will init the winsock stuff */
+          curl_global_init(CURL_GLOBAL_ALL);
 
+          /* get a curl handle */
+          curl = curl_easy_init();
+          if(curl) {
+            /* First set the URL that is about to receive our POST. This URL can
+               just as well be a https:// URL if that is what should receive the
+               data. */
             curl_easy_setopt(curl, CURLOPT_URL, "http://localhost/api/post");
             /* Now specify the POST data */
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"sensor_id\":" + 1 + ", \"value\":" + i + "}");
 
-
-            struct curl_slist *headers = NULL;
-            headers = curl_slist_append(headers, "Accept: application/json");
-            headers = curl_slist_append(headers, "Content-Type: application/json");
-            headers = curl_slist_append(headers, "charsets: utf-8");
-
-            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-            curl_easy_setopt(curl, CURLOPT_URL, curl_url);
-            curl_easy_setopt(curl, CURLOPT_POST, 1);
-            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, curl_fields);
-
+            /* Perform the request, res will get the return code */
             res = curl_easy_perform(curl);
 
             cout << res;
-
+            /* always cleanup */
             curl_easy_cleanup(curl);
-            curl_global_cleanup();
-        }
+          }
+          curl_global_cleanup();
     }
 }
