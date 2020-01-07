1b8e5509293723216cbe04ac9b076a455b54e4b7
PolFRX <pol.2frx@hotmail.fr>
2020-01-07 17:20:32 +0100

    update from prod

--- api/eHealthLib/main_bdd.cpp
+++ api/eHealthLib/main_bdd.cpp
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
             /* First set the URL that is about to receive our POST. This URL can
                just as well be a https:// URL if that is what should receive the
                data. */
-            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost/api/post");
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost/api/post/");
             /* Now specify the POST data */
             curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\"sensor_id\":" + std::to_string(1) + ", \"value\":" + std::to_string(i) + "}");
 
